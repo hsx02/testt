d4034b88eb06416d89fc114c31c31217b3b03c38
Arian <arian.kulmer@web.de>
2021-10-11 22:43:58 +0200

    sm6225-common: fingerprint: Uprev to 2.3 HAL
    Change-Id: I56ea1646ef464f7e533add5a0017620e21db686d
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- bengal.mk
+++ bengal.mk
@@ -181,7 +181,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_bengal
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_bengal
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
